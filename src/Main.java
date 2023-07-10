96d27bbe8af9fb612dd358ed277465a3316d8140
First project <ekulakov056@gmail.com>
2023-07-10 23:20:13 +0500

    Сделал первое задание

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        var dog = 3.5;
-        var cat = 1.6;
-        var paper = 7639;
         task1();
         task2();
         task3();
@@ -14,42 +11,47 @@ public class Main {
     }
 
     public static void task1 () {
+        var dog = 8.0;
+        var cat = 3.6;
+        var paper = 763789;
         System.out.println("Задача 1");
-        ... // Пишем код для задачи 1
+        System.out.println("dog = "+ dog);
+        System.out.println("cat = "+ cat);
+        System.out.println("paper = " + paper);
     }
 
     public static void task2 () {
         System.out.println("Задача 2");
-        ... // Пишем код для задачи 2
+        // Пишем код для задачи 2
     }
 
     public static void task3 () {
         System.out.println("Задача 3");
-        ... // Пишем код для задачи 1
+        // Пишем код для задачи 1
     }
 
     public static void task4 () {
         System.out.println("Задача 4");
-        ... // Пишем код для задачи 2
+        // Пишем код для задачи 2
     }
 
     public static void task5 () {
         System.out.println("Задача 5");
-        ... // Пишем код для задачи 1
+        // Пишем код для задачи 1
     }
 
     public static void task6 () {
         System.out.println("Задача 6");
-        ... // Пишем код для задачи 2
+        // Пишем код для задачи 2
     }
 
     public static void task7 () {
         System.out.println("Задача 7");
-        ... // Пишем код для задачи 1
+        // Пишем код для задачи 1
     }
 
     public static void task8 () {
         System.out.println("Задача 8");
-        ... // Пишем код для задачи 2
+        // Пишем код для задачи 2
     }
 }
